cfbe96de084604f1e4f5802a18b7ad1395699a86
David-Jess <djesse351@gmail.com>
2024-10-14 07:29:43 +0100

    made some changes to the styling of aboutus and homepage

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 const hamburger = document.querySelector(".hamburger");
-const navLinks = document.querySelector(".nav-right");
+const navLinks = document.querySelector(".mobile-menu");
+const mobileMenu = document.getElementById('mobile-menu')
+
+function closeMobileMenu() {
+    const mobileMenu = document.getElementById('mobile-menu')
+    if (mobileMenu) {
+        mobileMenu.removeAttribute('id');
+    }
+}
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
-    navLinks.classList.toggle("active");
+    //navLinks.classList.toggle("active");
+    mobileMenu.classList.toggle('active')
 });
 
-document.querySelectorAll(".nav-right").forEach(n => n.addEventListener("click", ()=> {
+document.getElementById("mobile-menu").forEach(n => n.addEventListener("click", ()=> {
     hamburger.classList.remove("active");
     navLinks.classList.remove("active");
+    mobileMenu.classList.remove("active")
 }))
